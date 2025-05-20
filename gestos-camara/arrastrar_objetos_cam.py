952762aefc4a480aa3ef7c189a3e316180544f4c
Kerwin2712 <kerwin27120201@gmail.com>
2025-05-20 09:57:44 -0400

    Doble control para activar el funcionamiento

--- gestos-camara/arrastrar_objetos_cam.py
+++ gestos-camara/arrastrar_objetos_cam.py
@@ -19,39 +19,66 @@ while True:
     hands, img_manos = detector.findHands(video, flipType=True)
     
     if hands:
-        hand = hands[0]
-        hand_type = hand["type"]  # 'Left' o 'Right'
-        lm_list = hand["lmList"]
-        
-        # Solo mano derecha
-        if hand_type == "Right":
-            # Coordenadas del ÍNDICE (punto 8) y PULGAR (punto 4)
-            index_x, index_y = lm_list[8][0], lm_list[8][1]  # Corregido
-            thumb_x, thumb_y = lm_list[4][0], lm_list[4][1]   # Corregido
+        hand = hands[0] 
+        hand_2 = hands[1] if len(hands) > 1 else None
+        if hand and hand_2:
+            hand_type = hand["type"] if hand else None # 'Left' o 'Right'
+            hand2_type = hand_2["type"] if hand_2 else None
             
-            # Convertir coordenadas a pantalla (invertir X por el flip)
-            scaled_x = int((video.shape[1] - index_x) / video.shape[1] * screen_w)
-            scaled_y = int(index_y  / video.shape[0] * screen_h)
-            
-            # Suavizar movimiento
-            scaled_x = prev_x + (scaled_x - prev_x) // smooth_factor
-            scaled_y = prev_y + (scaled_y - prev_y) // smooth_factor
-            
-            pyautogui.moveTo(scaled_x, scaled_y)
-            prev_x, prev_y = scaled_x, scaled_y
-            
-            # Calcular distancia entre dedos
-            distance = math.dist((index_x, index_y), (thumb_x, thumb_y))
-            
-            # Mantener clic presionado
-            if distance < click_threshold:
-                if not is_dragging:
-                    pyautogui.mouseDown()
-                    is_dragging = True
+            if hand_type == "Left":
+                hand_r = hand_2
+                hand_l = hand
             else:
-                if is_dragging:
-                    pyautogui.mouseUp()
-                    is_dragging = False
+                hand_r = hand
+                hand_l = hand_2
+            
+            
+            r_list = hand_r["lmList"] 
+            l_list = hand_l["lmList"] 
+        
+            if hand_l:
+                # Coordenadas del ÍNDICE (punto 8) y PULGAR (punto 4)
+                indexl_x, indexl_y = l_list[8][0], l_list[8][1]
+                thumbl_x, thumbl_y = l_list[4][0], l_list[4][1]
+                # Convertir coordenadas a pantalla (invertir X por el flip)
+                scaledl_x = int((video.shape[1] - indexl_x) / video.shape[1] * screen_w)
+                scaledl_y = int(indexl_y  / video.shape[0] * screen_h)
+                # Suavizar movimiento
+                scaledl_x = prev_x + (scaledl_x - prev_x) // smooth_factor
+                scaledl_y = prev_y + (scaledl_y - prev_y) // smooth_factor
+                
+                distancel = math.dist((indexl_x, indexl_y), (thumbl_x, thumbl_y))
+                
+                if distancel < click_threshold:
+                    # Solo mano derecha
+                    if hand_r:
+                        # Coordenadas del ÍNDICE (punto 8) y PULGAR (punto 4)
+                        indexr_x, indexr_y = r_list[8][0], r_list[8][1]  # Corregido
+                        thumbr_x, thumbr_y = r_list[4][0], r_list[4][1]   # Corregido
+                        
+                        # Convertir coordenadas a pantalla (invertir X por el flip)
+                        scaledr_x = int((video.shape[1] - indexr_x) / video.shape[1] * screen_w)
+                        scaledr_y = int(indexr_y  / video.shape[0] * screen_h)
+                        
+                        # Suavizar movimiento
+                        scaledr_x = prev_x + (scaledr_x - prev_x) // smooth_factor
+                        scaledr_y = prev_y + (scaledr_y - prev_y) // smooth_factor
+                        
+                        pyautogui.moveTo(scaledr_x, scaledr_y)
+                        prev_x, prev_y = scaledr_x, scaledr_y
+                        
+                        # Calcular distancia entre dedos
+                        distancer = math.dist((indexr_x, indexr_y), (thumbr_x, thumbr_y))
+                        
+                        # Mantener clic presionado
+                        if distancer < click_threshold:
+                            if not is_dragging:
+                                pyautogui.mouseDown()
+                                is_dragging = True
+                        else:
+                            if is_dragging:
+                                pyautogui.mouseUp()
+                                is_dragging = False
 
     cv2.imshow('webcam', img_manos)
     if cv2.waitKey(1) != -1:
